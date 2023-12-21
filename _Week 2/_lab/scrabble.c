949e4a12d874464c0720a8fc08f8fe2102325880
Lucas Rocha <lucas.edr27@gmail.com>
2023-12-21 16:55:13 -0300

    Update scrabble.c

--- _Week 2/_lab/scrabble.c
+++ _Week 2/_lab/scrabble.c
@@ -1,32 +1,88 @@
 #include <ctype.h>
-#include <cs50.h>
 #include <stdio.h>
 #include <string.h>
 
-// Points assigned to each letter of the alphabet
-int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-
-int compute_score(string word);
-
 int main(void)
 {
-    // Get input words from both players
-    string word1 = get_string("Player 1: ");
-    string word2 = get_string("Player 2: ");
+    // declara funcao e variaveis
+    int pontos(char resposta[]);
+    char primeiraResposta[90], segundaResposta[90];
+    int pontuacaoPlayer1, pontuacaoPlayer2;
 
-    // Score both words
-    int score1 = compute_score(word1);
-    int score2 = compute_score(word2);
+    // player insere sua palavra
+    printf("Player 1: ");
+    scanf("%s", primeiraResposta);
+    printf("Player 2: ");
+    scanf("%s", segundaResposta);
 
-    // TODO: Print the winner
-    if (score1 > score2) {
-        printf("Player 1 wins!");
-    } else {
-        printf("Player 2 wins!");
+    // chama as funcoes
+    int scorePlayer1 = pontos(primeiraResposta);
+    int scorePlayer2 = pontos(segundaResposta);
+
+    // print pontos
+    printf("%i\n", scorePlayer1);
+    printf("%i\n", scorePlayer2);
+
+    // diz quem venceu
+    if (scorePlayer1 > scorePlayer2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (scorePlayer1 == scorePlayer2)
+    {
+        printf("Tie!\n");
+    }
+    else
+    {
+        printf("Player 2 wins!\n");
     }
 }
 
-int compute_score(string word)
+int pontos(char resposta[])
 {
-    // TODO: Compute and return score for string
+    int pontuacao = 0;
+    int quantLetras = strlen(resposta);
+    for (int cont = 0; cont < quantLetras; cont++)
+    {
+        if (resposta[cont] == toupper(resposta[cont]))
+        {
+            resposta[cont] = tolower(resposta[cont]);
+        }
+        if ((resposta[cont] == 'a') || (resposta[cont] == 'e') || (resposta[cont] == 'i') || (resposta[cont] == 'l') ||
+            (resposta[cont] == 'n') || (resposta[cont] == 'o') || (resposta[cont] == 'r') || (resposta[cont] == 's') ||
+            (resposta[cont] == 't') || (resposta[cont] == 'u'))
+        {
+            pontuacao += 1;
+        }
+        else if ((resposta[cont] == 'd') || (resposta[cont] == 'g'))
+        {
+            pontuacao += 2;
+        }
+        else if ((resposta[cont] == 'b') || (resposta[cont] == 'c') || (resposta[cont] == 'm') || (resposta[cont] == 'p'))
+        {
+            pontuacao += 3;
+        }
+        else if ((resposta[cont] == 'f') || (resposta[cont] == 'h') || (resposta[cont] == 'v') || (resposta[cont] == 'w') ||
+                 (resposta[cont] == 'y'))
+        {
+            pontuacao += 4;
+        }
+        else if (resposta[cont] == 'k')
+        {
+            pontuacao += 5;
+        }
+        else if ((resposta[cont] == 'j') || (resposta[cont] == 'x'))
+        {
+            pontuacao += 8;
+        }
+        else if ((resposta[cont] == 'q') || (resposta[cont] == 'z'))
+        {
+            pontuacao += 10;
+        }
+        else
+        {
+            pontuacao += 0;
+        }
+    }
+    return pontuacao;
 }
